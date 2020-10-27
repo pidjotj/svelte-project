81c84e2a196d7636de9b01ac95cb9cca4bc59b4f
Jeremovic <j.pidjot@hotmail.fr>
2020-10-27 17:18:22 +0100

    commit lesson 3 - 4 - 5

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,37 @@
 <script>
-	export let name;
+	let firstName = "Jerem";
+	let lastName = "Pidjot";
+	let beltColor = "White";
+
+	// $: is the way to declare a "reactive value" it will be update when the value change.
+	// It can be use on a console log as the example below -> a log will be trigger every time 'beltColor' change
+	$: fullName = `${firstName} ${lastName}`;
+	$: console.log(beltColor);
+	// a reactive value can take multiple statements
+	$: {
+		console.log(fullName);
+		console.log(beltColor);
+	}
+
+	const handleClick = () => {
+		beltColor = 'blue';
+	};
+
+	// Tip : Useless because of the bind:value
+	// const handleInput = event => {
+	// 	beltColor = event.target.value;
+	// }
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>Hello {fullName}!</h1>
+	<p style="color: {beltColor}">{beltColor} belt</p>
+	<!--<input type="text" on:input={handleInput} value={beltColor}>-->
+	<!-- bind:value -> on:input= & value= at the same time {strong} -->
+
+	<input type="text" bind:value={firstName} >
+	<input type="text" bind:value={lastName} >
+	<input type="text" bind:value={beltColor} >
 </main>
 
 <style>
